1c4c2f042f8deefb3d99a4f41904a4edc7d2b582
Mateusz Michnieski <m.michniewski@wb.com.pl>
2017-07-30 16:17:13 +0200

    Symulowanie2

--- MainWindow.cc
+++ MainWindow.cc
@@ -7,13 +7,111 @@ MainWindow::MainWindow(QWidget *parent) :
 		ui(new Ui::MainWindow),
 		ktoryBalon(0) {
 	ui->setupUi(this);
-	ui->labelAuto->show ();
+	srand(time(0));
+	aktualnyTryb = NONE;
+	ui->labelAuto->setVisible(false);
 	ui->pushButton->setFocus ();
-	ui->labelBal1->hide ();
-	ui->labelBal2->hide ();
-	ui->labelBal3->hide ();
+	ui->labelBal1->setVisible(false);
+	ui->labelBal2->setVisible(false);
+	ui->labelBal3->setVisible(false);
 	timerBalonowy = new QTimer(this);
 	connect(timerBalonowy, SIGNAL(timeout()), this, SLOT(bujajBalonem()));
+
+	QIcon a("://zas/a_trans.png");
+	QIcon u("://zas/U.png");
+	QIcon d("://zas/D.png");
+	QIcon i("://zas/i_trans.png");
+	ikony["A"] = a;
+	ikony.insert ("U", u);
+	ikony["D"] = d;
+	ikony.insert ("I", i );
+
+	przyciski.append (ui->pushButton0);
+	przyciski.append (ui->pushButton1);
+	przyciski.append (ui->pushButton2);
+	przyciski.append (ui->pushButton3);
+	przyciski.append (ui->pushButton4);
+	przyciski.append (ui->pushButton5);
+	przyciski.append (ui->pushButton6);
+	przyciski.append (ui->pushButton7);
+	przyciski.append (ui->pushButton8);
+	przyciski.append (ui->pushButton9);
+	przyciski.append (ui->pushButton10);
+	przyciski.append (ui->pushButton11);
+	przyciski.append (ui->pushButton12);
+	przyciski.append (ui->pushButton13);
+	przyciski.append (ui->pushButton14);
+
+		motywAudi ();
+}
+
+void MainWindow::motywAudi() {
+	wylosuj4Liczby ();
+
+	przyciski[wylosowane[0]]->setIcon (ikony["A"]);
+	przyciski[wylosowane[1]]->setIcon (ikony["U"]);
+	przyciski[wylosowane[2]]->setIcon (ikony["D"]);
+	przyciski[wylosowane[3]]->setIcon (ikony["I"]);
+
+
+	int *tab = wylosowane;
+	qDebug() << *tab;
+	qDebug() << *(tab+1);
+	qDebug() << *(tab+2);
+	qDebug() << *(tab+3);
+}
+
+void MainWindow::on_pushButtonStart_clicked() {
+	ui->pushButtonStart->hide ();
+	ui->labelAuto->show ();
+	wlaczBujanieBalonem();
+	QPixmap audi(":/auta/ford04.jpeg"); // ford 02 03 .png 01 jpg 04jpeg
+	ui->labelAuto->setPixmap(audi);
+//	ui->pushButtonA->setIcon (ikony["A"]);
+//	ui->pushButtonA->setToolTip ("A");
+
+//	if (ui->pushButtonA->toolTip () == QString("A")) {
+//		exit(1);
+		//ui->pushButtonA->setToolTip ("");
+//	}
+}
+
+//	ui->pushButtonA->setVisible (false);
+//	pierwsza = ui->pushButtonA;
+//	QPixmap a(":/zas/a_trans.png");
+//	ui->label->setPixmap (a);
+
+void MainWindow::on_labelBal3_clicked()
+{
+	exit(1);
+}
+
+void MainWindow::on_labelBal1_clicked()
+{
+	exit(1);
+}
+
+void MainWindow::on_labelBal2_clicked()
+{
+	exit(1);
+}
+
+void MainWindow::bujajBalonem() {
+	switch (ktoryBalon) {
+		case 0:
+			pokazBalon1 ();
+			break;
+		case 1:
+			pokazBalon2 ();
+			break;
+		case 2:
+			pokazBalon3 ();
+			break;
+		}
+}
+
+void MainWindow::wlaczBujanieBalonem() {
+	timerBalonowy->start(520);
 }
 
 MainWindow::~MainWindow()
@@ -31,47 +129,51 @@ void MainWindow::resizeEvent(QResizeEvent *evt) {
 	QMainWindow::resizeEvent(evt);
 }
 
-void MainWindow::on_pushButtonStart_clicked() {
-	ui->pushButtonStart->hide ();
-	ui->labelAuto->show ();
-	wlaczBujanieBalonem();
+bool MainWindow::czyBylaWylosowana( int iLiczba, int tab[], int ile ) {
+	if( ile <= 0 ) return false;
+	int i = 0;
+
+	do {
+		if( tab[ i ] == iLiczba ) return true;
+		i++;
+	} while( i < ile );
+
+	return false;
 }
 
-void MainWindow::bujajBalonem() {
-	switch (ktoryBalon) {
-	case 0:
-		pokazBalon1 ();
-		break;
-	case 1:
-		pokazBalon2 ();
-		break;
-	case 2:
-		pokazBalon3 ();
-		break;
-	}
+int MainWindow::wylosuj() {
+	return( rand() % 15 );
 }
 
-void MainWindow::wlaczBujanieBalonem() {
-	timerBalonowy->start(520);
+void MainWindow::wylosuj4Liczby() {
+		int wylosowanych = 0;
+	do {
+		int liczba = wylosuj();
+		if( czyBylaWylosowana( liczba, wylosowane, wylosowanych ) == false )
+		{
+			wylosowane[ wylosowanych ] = liczba;
+			wylosowanych++;
+		}
+	} while( wylosowanych < 4);
 }
 
 void MainWindow::pokazBalon1() {
-	ui->labelBal1->show ();
-	ui->labelBal2->hide ();
-	ui->labelBal3->hide ();
+	ui->labelBal1->setVisible(true);
+	ui->labelBal2->setVisible(false);
+	ui->labelBal3->setVisible(false);
 	ktoryBalon = 1;
 }
 
 void MainWindow::pokazBalon2() {
-	ui->labelBal1->hide ();
-	ui->labelBal2->show ();
-	ui->labelBal3->hide ();
+	ui->labelBal1->setVisible(false);
+	ui->labelBal2->setVisible(true);
+	ui->labelBal3->setVisible(false);
 	ktoryBalon = 2;
 }
 
 void MainWindow::pokazBalon3() {
-	ui->labelBal1->hide ();
-	ui->labelBal2->hide ();
-	ui->labelBal3->show ();
+	ui->labelBal1->setVisible(false);
+	ui->labelBal2->setVisible(false);
+	ui->labelBal3->setVisible(true);
 	ktoryBalon = 0;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -3,13 +3,22 @@
 
 #include <QMainWindow>
 #include <QTimer>
+#include <QPushButton>
+#include <QMap>
+//#include"time.h"
+#include <cstdlib>           //rand()
 
 namespace Ui {
 class MainWindow;
 }
+enum tryb {
+	NONE,
+	AUDI,
+	FORD,
+	FIAT,
+};
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow{
 		Q_OBJECT
 
 	public:
@@ -20,13 +29,30 @@ class MainWindow : public QMainWindow
 		void on_pushButtonStart_clicked();
 		void bujajBalonem();
 
+		void on_labelBal3_clicked();
+		void on_labelBal1_clicked();
+		void on_labelBal2_clicked();
+
 	private:
+		void motywAudi();
+
+		QMap<QString, QIcon>ikony;
+		QList<QPushButton *> przyciski;
+
+		tryb aktualnyTryb;
+		QPushButton *wynikpierwsza;
+		QPushButton *wynikdruga;
+		QPushButton *wyniktrzecia;
+		QPushButton *wynikczwarta;
+int wylosowane[4];
+		bool czyBylaWylosowana( int iLiczba, int tab[], int ile );
+		int wylosuj();
+		void wylosuj4Liczby();
+		void resizeEvent(QResizeEvent *evt);
 		void wlaczBujanieBalonem();
 		void pokazBalon1();
 		void pokazBalon2();
 		void pokazBalon3();
-		void resizeEvent(QResizeEvent *evt);
-
 		QTimer *timerBalonowy;
 		Ui::MainWindow *ui;
 		int ktoryBalon;
