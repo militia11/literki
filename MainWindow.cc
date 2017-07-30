73b4de41d31d8f414fbd97dab687a614150c4ad6
Mateusz Michnieski <m.michniewski@wb.com.pl>
2017-07-30 21:33:01 +0200

    Symulowanie 5

--- MainWindow.cc
+++ MainWindow.cc
@@ -5,21 +5,32 @@
 MainWindow::MainWindow(QWidget *parent) :
 		QMainWindow(parent),
 		ui(new Ui::MainWindow),
-		ktoryBalon(0) {
+		ktoryBalon(0),
+		trafionychLiter(0),
+		victoryTimcounter(0){
 	ui->setupUi(this);
 	srand(time(0));
 	aktualnyTryb = NONE;
 	QFont f( "Arial", 106, QFont::Bold);
 	ui->label->setStyleSheet("QLabel {color : red; }");
 	ui->label->setFont (f);
+	ui->label->setVisible (false);
 	ui->labelAuto->setVisible(false);
 	ui->pushButton->setFocus ();
+
 	ui->labelBal1->setVisible(false);
 	ui->labelBal2->setVisible(false);
 	ui->labelBal3->setVisible(false);
 	timerBalonowy = new QTimer(this);
 	connect(timerBalonowy, SIGNAL(timeout()), this, SLOT(bujajBalonem()));
 
+	timerVictory = new QTimer(this);
+	connect(timerVictory, SIGNAL(timeout()), this, SLOT(victory()));
+
+	labelyWAucie.append (ui->label1);
+	labelyWAucie.append (ui->label2);
+	labelyWAucie.append (ui->label3);
+	labelyWAucie.append (ui->label4);
 	QIcon a("://zas/a_trans.png");
 	QIcon u("://zas/U.png");
 	QIcon d("://zas/D.png");
@@ -29,6 +40,21 @@ MainWindow::MainWindow(QWidget *parent) :
 	ikony["D"] = d;
 	ikony.insert ("I", i );
 
+	QIcon ff("://zas/F.png");
+	QIcon o("://zas/Oo.png");
+	QIcon r("://zas/R.png");
+	ikony.insert ("F", ff);
+	ikony["O"] = o;
+	ikony.insert ("R", r );
+
+	QIcon p("://zas/p_trans.png");
+	ikony.insert ("P", p);
+	QIcon e("://zas/EE.png");
+	ikony["E"] = e;
+	QIcon ll("://zas/giphy.gif");
+	ikony["L"]= ll;
+	QIcon t("://zas/t.png");
+	ikony["T"] = t;
 	slownikTrybow[AUDI] = "audi";
 	slownikTrybow[FORD] = "ford";
 	slownikTrybow[FIAT] = "fiat";
@@ -50,23 +76,101 @@ MainWindow::MainWindow(QWidget *parent) :
 	przyciski.append (ui->pushButton13);
 	przyciski.append (ui->pushButton14);
 
+	for (int i = 0; i < 15;i++) {
+		connect(
+			przyciski.at (i),
+			SIGNAL(clicked()),
+			SLOT(sprawdzLitereNaPrzycisku()));
+	}
 	pobierzIdNastepnegoAuta ();
 }
 
-void MainWindow::trybAudi() {
+void MainWindow::uzupelnijPrzyciskiLiterami() {
+	for(int i=0; i<4;i++) {
+		przyciski[wylosowaneLiczbyDoPrzyciskow[i]]->
+			setIcon(ikony[literyMarki.at (i)]);
+		przyciski[wylosowaneLiczbyDoPrzyciskow[i]]->
+			setToolTip (literyMarki.at (i));
+	}
+	aktualizujAktywnaLitere();
+}
+
+void MainWindow::victory() {
+	foreach (QLabel *l, labelyWAucie) {
+		l->setVisible (!l->isVisible ());
+	}
+
+	ui->labelPanda->setVisible(!ui->labelPanda->isVisible ());
+	victoryTimcounter++;
+
+	if (victoryTimcounter == 5) {
+		timerVictory->stop ();
+		victoryTimcounter =0;
+		ui->labelAuto->setVisible (true);
+		foreach (QLabel*l, labelyWAucie) {
+			l->setVisible (true);
+			l->clear ();
+		}
+		odkryjNaNowoPrzyciskiZPoszukiwanych();
+		ustawNastepneAuto ();
+	}
+}
+
+void MainWindow::ustawZdjecieLabVictory() {
+	int id = wylosuj(3);
+}
+
+void MainWindow::sprawdzLitereNaPrzycisku() {
+	QPushButton *but =qobject_cast<QPushButton *>(QObject::sender());
+	if (but->toolTip () == ui->label->text ()) {
+		// TRAFIONY DZWIEK !!!!!!!!!!
+		przeniesLitereDoZdjAuta (but);
+		but->hide ();
+		trafionychLiter++;
+		if (trafionychLiter == 4) {
+			ui->labelPanda->setVisible (true);
+			ui->labelAuto->hide ();
+			ustawZdjecieLabVictory();
+			timerVictory->start(660);
+			trafionychLiter = 0;
+			ui->label->hide ();
+
+		} else {
+			aktualizujAktywnaLitere();
+		}
+	} else {
+		// NIETRAFIONY DZWIEK !!!!!!!!!!
+	}
+}
+
+void MainWindow::przeniesLitereDoZdjAuta(QPushButton *but) {
+	labelyWAucie[trafionychLiter]->
+		setPixmap (ikony[but->toolTip ()].pixmap (QSize(132, 132)));
+}
+
+void MainWindow::ustawLiteryWPrzyciskach() {
 	wylosuj4Liczby ();
-	przyciski[wylosowaneLivzbyDoPrzyciskow[0]]->setIcon (ikony["A"]);
-	przyciski[wylosowaneLivzbyDoPrzyciskow[1]]->setIcon (ikony["U"]);
-	przyciski[wylosowaneLivzbyDoPrzyciskow[2]]->setIcon (ikony["D"]);
-	przyciski[wylosowaneLivzbyDoPrzyciskow[3]]->setIcon (ikony["I"]);
+	switch (aktualnyTryb) {
+	case AUDI:
+		wyswietlLiteryDlaAudi();
+		break;
+	case FIAT:
+		wyswietlLiteryDlaFiat ();
+		break;
+	case FORD:
+		wyswietlLiteryDlaFord ();
+		break;
+	case OPEL:
+		wyswietlLiteryDlaOpel ();
+		break;
+	default:
+		break;
+	}
+}
 
-	//	ui->pushButtonA->setIcon (ikony["A"]);
-//	ui->pushButtonA->setToolTip ("A");
-
-//	if (ui->pushButtonA->toolTip () == QString("A")) {
-//		exit(1);
-		//ui->pushButtonA->setToolTip ("");
-//	}
+void MainWindow::aktualizujAktywnaLitere() {
+	QString letter = literyMarki.at (trafionychLiter);
+	ui->label->setText (letter);
 }
 
 int MainWindow::pobierzIdNastepnegoAuta() {
@@ -75,12 +179,22 @@ int MainWindow::pobierzIdNastepnegoAuta() {
 		vtryb = wylosuj (4);
 	} while (vtryb == static_cast<int>(aktualnyTryb));
 	aktualnyTryb = static_cast<tryb>(vtryb);
+
 	return wylosuj (4);
+}
+
+
+
+void MainWindow::odkryjNaNowoPrzyciskiZPoszukiwanych() {
+	for(int i=0; i<4;i++) {
+		przyciski[wylosowaneLiczbyDoPrzyciskow[i]]->setVisible (true);
+	}
 }
 
 void MainWindow::on_pushButtonStart_clicked() {
 	ui->pushButtonStart->hide ();
 	ui->labelAuto->show ();
+	ui->label->setVisible (true);
 	wlaczBujanieBalonem();
 	ustawNastepneAuto ();
 }
@@ -103,11 +217,10 @@ void MainWindow::ustawNastepneAuto() {
 	int id = pobierzIdNastepnegoAuta ();
 	QString path = ":/auta/";
 	path+= slownikTrybow[aktualnyTryb];
-	qDebug() << aktualnyTryb << slownikTrybow[aktualnyTryb];
+	ustawLiteryWPrzyciskach();
 	path+=QString::number(id);
 	path+=".jpg";
 	ui->labelAuto->setPixmap(QPixmap(path));
-	qDebug() << path;
 }
 
 void MainWindow::bujajBalonem() {
@@ -162,12 +275,48 @@ void MainWindow::wylosuj4Liczby() {
 		int wylosowanych = 0;
 	do {
 		int liczba = wylosuj(15);
-		if( czyBylaWylosowana( liczba, wylosowaneLivzbyDoPrzyciskow, wylosowanych ) == false )
+		if( czyBylaWylosowana( liczba, wylosowaneLiczbyDoPrzyciskow, wylosowanych ) == false )
 		{
-			wylosowaneLivzbyDoPrzyciskow[ wylosowanych ] = liczba;
+			wylosowaneLiczbyDoPrzyciskow[ wylosowanych ] = liczba;
 			wylosowanych++;
 		}
 	} while( wylosowanych < 4);
+}
+
+void MainWindow::wyswietlLiteryDlaAudi() {
+	literyMarki.clear ();
+	literyMarki.append ("A");
+	literyMarki.append ("U");
+	literyMarki.append ("D");
+	literyMarki.append ("I");
+	uzupelnijPrzyciskiLiterami ();
+}
+
+void MainWindow::wyswietlLiteryDlaFiat() {
+	literyMarki.clear ();
+	literyMarki.append ("F");
+	literyMarki.append ("I");
+	literyMarki.append ("A");
+	literyMarki.append ("T");
+	uzupelnijPrzyciskiLiterami ();
+}
+
+void MainWindow::wyswietlLiteryDlaFord() {
+	literyMarki.clear ();
+	literyMarki.append ("F");
+	literyMarki.append ("O");
+	literyMarki.append ("R");
+	literyMarki.append ("D");
+	uzupelnijPrzyciskiLiterami ();
+}
+
+void MainWindow::wyswietlLiteryDlaOpel() {
+	literyMarki.clear ();
+	literyMarki.append ("O");
+	literyMarki.append ("P");
+	literyMarki.append ("E");
+	literyMarki.append ("L");
+	uzupelnijPrzyciskiLiterami ();
 }
 
 void MainWindow::pokazBalon1() {
--- MainWindow.h
+++ MainWindow.h
@@ -7,6 +7,7 @@
 #include <QMap>
 #include <QHash>
 #include <cstdlib>           //rand()
+#include <QLabel>
 
 namespace Ui {
 class MainWindow;
@@ -28,6 +29,9 @@ class MainWindow : public QMainWindow{
 
 	private slots:
 		void on_pushButtonStart_clicked();
+		void sprawdzLitereNaPrzycisku();
+
+		void victory();
 		void bujajBalonem();
 
 		void on_labelBal3_clicked();
@@ -36,23 +40,40 @@ class MainWindow : public QMainWindow{
 
 	private:
 		void ustawNastepneAuto();
-		void trybAudi();
-
+		void ustawLiteryWPrzyciskach();
 		int pobierzIdNastepnegoAuta();
 		tryb aktualnyTryb;
 
+		void ustawZdjecieLabVictory();
+		int trafionychLiter;
+		QList<QString> literyMarki;
+		QList<QLabel *> labelyWAucie;
 		QHash<tryb, QString> slownikTrybow;
 		QHash<QString, QIcon>ikony;
-		QList<QPushButton *> przyciski;
 
+		QList<QPushButton *> przyciski;
 		QPushButton *wynikpierwsza;
 		QPushButton *wynikdruga;
 		QPushButton *wyniktrzecia;
 		QPushButton *wynikczwarta;
-		int wylosowaneLivzbyDoPrzyciskow[4];
+
+		void odkryjNaNowoPrzyciskiZPoszukiwanych();
+		void uzupelnijPrzyciskiLiterami();
+		void przeniesLitereDoZdjAuta(QPushButton *but);
+		void aktualizujAktywnaLitere();
+
+		void wyswietlLiteryDlaAudi();
+		void wyswietlLiteryDlaFiat();
+		void wyswietlLiteryDlaFord();
+		void wyswietlLiteryDlaOpel();
+
+		QTimer *timerVictory;
+
+		int wylosowaneLiczbyDoPrzyciskow[4];
 		bool czyBylaWylosowana( int iLiczba, int tab[], int ile );
 		int wylosuj(int zIlu);
 		void wylosuj4Liczby();
+		int victoryTimcounter;
 		void resizeEvent(QResizeEvent *evt);
 		void wlaczBujanieBalonem();
 		void pokazBalon1();
